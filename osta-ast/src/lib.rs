8b0fa218804bb7cf5b316524c16df34d5175481c
JohanVonElectrum <joanestradium@gmail.com>
2024-04-30 21:20:30 +0200

    Parser design

--- osta-ast/src/lib.rs
+++ osta-ast/src/lib.rs
@@ -23,7 +23,55 @@ pub struct Node {
     parent: NodeRef,
 }
 
-pub struct Tree<'a> {
+pub struct Ast<'a> {
     nodes: Vec<Node>,
     datas: Vec<Data<'a>>
+}
+
+pub struct AstBuilder<'a> {
+    ast: Ast<'a>,
+    checkpoints: Vec<(NodeRef, DataRef)>
+}
+
+impl<'a> AstBuilder<'a> {
+    pub fn new() -> Self {
+        Self {
+            ast: Ast {
+                nodes: Vec::new(),
+                datas: Vec::new()
+            },
+            checkpoints: Vec::new()
+        }
+    }
+
+    pub fn checkpoint(&mut self) {
+        self.checkpoints.push((self.ast.nodes.len(), self.ast.datas.len()));
+    }
+
+    pub fn rollback<Err>(&mut self, err: Err) -> Result<(), Err> {
+        let (node_len, data_len) = self.checkpoints.pop().ok_or(err)?;
+        self.ast.nodes.truncate(node_len);
+        self.ast.datas.truncate(data_len);
+        Ok(())
+    }
+
+    pub fn commit(&mut self) {
+        self.checkpoints.pop().or_else(|| panic!("No checkpoints to commit"));
+    }
+
+    pub fn push_node(&mut self, kind: NodeKind, parent: NodeRef) -> NodeRef {
+        let node_ref = self.ast.nodes.len();
+        self.ast.nodes.push(Node { kind, parent });
+        node_ref
+    }
+
+    pub fn push_data(&mut self, data: Data<'a>) -> DataRef {
+        let data_ref = self.ast.datas.len();
+        self.ast.datas.push(data);
+        data_ref
+    }
+
+    pub fn build(self) -> Ast<'a> {
+        self.ast
+    }
 }
---
+++ osta-parser/src/lib.rs
@@ -0,0 +1,47 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use osta_ast::{AstBuilder, DataRef, NodeRef};
+use osta_func::{FallibleStateMonad, StateMonad};
+use osta_lexer::token::TokenKind;
+
+#[derive(Clone)]
+struct ParserInput<'a> {
+    input: &'a str,
+    builder: Rc<RefCell<AstBuilder<'a>>>
+}
+type ParserOutput<'a> = (NodeRef, Option<DataRef>);
+
+pub enum ParserError {
+    UnexpectedToken { expected: TokenKind, found: TokenKind },
+}
+
+pub trait Parser<'a>: FallibleStateMonad<'a, ParserInput<'a>, ParserOutput<'a>, ParserError> {
+    fn or<P: Parser<'a> + 'a>(self, parser: P) -> impl Parser<'a> {
+        move |input: ParserInput<'a>| {
+            let mut builder = input.builder.borrow_mut();
+            builder.checkpoint();
+            let (result, rest) = self.apply(input.clone());
+            match result {
+                Ok(out) => {
+                    builder.commit();
+                    (Ok(out), rest)
+                },
+                Err(err) => {
+                    if let Err(err) = builder.rollback(err) {
+                        return (Err(err), input.clone());
+                    }
+                    let (result, rest) = parser.apply(input.clone());
+                    match result {
+                        Ok(out) => (Ok(out), rest),
+                        Err(err) => (Err(err), input.clone())
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<'a, M> Parser<'a> for M
+where
+    M: FallibleStateMonad<'a, ParserInput<'a>, ParserOutput<'a>, ParserError>
+{}
